6a6f1e6628160f672ea762c1d8311ac8a91147ec
Sameer Zaher <sameerzaher@gmail.com>
2021-10-01 13:53:02 +0300

    test

--- matazim- BACKEND/main/serializers.py
+++ matazim- BACKEND/main/serializers.py
@@ -12,4 +12,4 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
-        return user 
+        return user
