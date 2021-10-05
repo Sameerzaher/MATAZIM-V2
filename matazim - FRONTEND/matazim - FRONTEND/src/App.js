d933e63a5b5e437cb4e98a6d81975a9e94189b81
yarin <yarintz5@gmail.com>
2021-10-05 12:15:25 +0300

    new

---
+++ matazim - FRONTEND/matazim - FRONTEND/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React, {useEffect} from 'react';
+import Navbar from "./components/Navbar";
+import HomepageScreen from './pages/HomepageScreen';
+import CoursesScreen from './pages/CoursesScreen';
+import ProjectsScreen from './pages/ProjectsScreen';
+import ProfileScreen from './pages/ProfileScreen';
+
+import Signin from './pages/Signin'; 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useCookies } from 'react-cookie';
+
+
+
+function App() {
+  const [token, setToken, deleteToken] = useCookies(['mr-token']);
+
+
+  return (
+
+    <Router>
+     
+      <Navbar />
+      <Switch>
+        <Route path="/HomepageScreen" exact component={HomepageScreen} />
+        <Route path="/CoursesScreen" component={CoursesScreen} />
+        <Route path="/ProjectsScreen" component={ProjectsScreen} />
+        <Route path="/ProfileScreen" component={ProfileScreen} />
+        <Route path="/Signin" component={Signin} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
